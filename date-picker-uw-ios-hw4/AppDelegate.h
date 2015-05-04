3a98c3a68d9371297513b491557bad02b2f18d1e
Carl Parker <Carl.Parker@hbo.com>
2015-05-04 08:11:53 -0700

    Initial Commit

---
+++ date-picker-uw-ios-hw4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  date-picker-uw-ios-hw4
+//
+//  Created by Parker, Carl (HBO) on 5/4/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ date-picker-uw-ios-hw4/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  date-picker-uw-ios-hw4
+//
+//  Created by Parker, Carl (HBO) on 5/4/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
