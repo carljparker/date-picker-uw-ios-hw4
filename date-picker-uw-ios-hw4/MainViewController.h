621269906784fe4f48b4e121483d2e4982d20c31
Carl Parker <Carl.Parker@hbo.com>
2015-05-04 18:25:30 -0700

    remove storyboard and add my own view controller

--- date-picker-uw-ios-hw4/MainViewController.h
+++ date-picker-uw-ios-hw4/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  date-picker-uw-ios-hw4
 //
 //  Created by Parker, Carl (HBO) on 5/4/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
