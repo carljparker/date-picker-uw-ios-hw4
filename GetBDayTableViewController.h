96a2a1fdb9510dfa0ca2b3aeabf1da43ad39a02f
Carl Parker <Carl.Parker@hbo.com>
2015-05-14 13:28:53 -0400

    add backing class for static cells. requires that dynamic table infra code be commented out or deleted

---
+++ GetBDayTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetBDayTableViewController.h
+//  date-picker-uw-ios-hw4
+//
+//  Created by Parker, Carl (HBO) on 5/14/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetBDayTableViewController : UITableViewController
+
+@end
---
+++ GetBDayViewController.m
@@ -0,0 +1,37 @@
+//
+//  GetBDayViewController.m
+//  date-picker-uw-ios-hw4
+//
+//  Created by Parker, Carl (HBO) on 5/14/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import "GetBDayViewController.h"
+
+@interface GetBDayViewController ()
+
+@end
+
+@implementation GetBDayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
